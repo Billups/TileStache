336cee2ae9276d47429e9021df47801ccf27a8fa
Seth van Ommen <seth.vanommen@billups.com>
2019-06-20 16:16:06 -0700

    working Rclone container, Dockerfile.rclone

--- scripts/billups/entrypoint.sh
+++ scripts/billups/entrypoint.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-TILEGEN_REV="$(rclone lsd s3:billups-artifacts/tilegen | tail -n1 | awk '{print $5}')"
-TILEGEN_VER="$(rclone ls s3:billups-artifacts/tilegen/$TILEGEN_REV/products | grep ".edge" | tail -n1 | awk '{print $2}')"
-rclone copy -v --stats 5s s3:billups-artifacts/tilegen/$TILEGEN_REV/products/$TILEGEN_VER .
-tar xfv $TILEGEN_VER -C /usr/src/data/
-rm $TILEGEN_VER
+confd -onetime -backend ssm --prefix /${ENVIRONMENT} -node https://ssm.us-west-2.amazonaws.com
+
+TILEGEN_REV="$(rclone lsd remote:billups-artifacts/tilegen | tail -n1 | awk '{print $5}')"
+TILEGEN_VER="$(rclone ls remote:billups-artifacts/tilegen/$TILEGEN_REV/products | grep ".edge" | tail -n1 | awk '{print $2}')"
+rclone copy -vv --stats 5s remote:billups-artifacts/tilegen/$TILEGEN_REV/products/$TILEGEN_VER /usr/src/data/
+tar xfv /usr/src/data/$TILEGEN_VER -C /usr/src/data/
+exec rm /usr/src/data/$TILEGEN_VER
