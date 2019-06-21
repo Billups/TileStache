d08c7a385814bff2fee65ed42b3900fd5cb98981
Seth van Ommen <seth.vanommen@billups.com>
2019-06-20 18:46:32 -0700

    rclone on container start seems better for now

--- scripts/billups/entrypoint.sh
+++ scripts/billups/entrypoint.sh
@@ -8,4 +8,5 @@ TILEGEN_REV="$(rclone lsd remote:billups-artifacts/tilegen | tail -n1 | awk '{pr
 TILEGEN_VER="$(rclone ls remote:billups-artifacts/tilegen/$TILEGEN_REV/products | grep ".edge" | tail -n1 | awk '{print $2}')"
 rclone copy -vv --stats 5s remote:billups-artifacts/tilegen/$TILEGEN_REV/products/$TILEGEN_VER /usr/src/data/
 tar xfv /usr/src/data/$TILEGEN_VER -C /usr/src/data/
-exec rm /usr/src/data/$TILEGEN_VER
+# exec rm /usr/src/data/$TILEGEN_VER
+exec gunicorn -c gunicorn/config.py.sample -b 0.0.0.0:9090 "TileStache:WSGITileServer('tilestache.cfg.sample')"
